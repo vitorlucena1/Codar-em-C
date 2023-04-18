547404ae7fbe9a1ca33dc0f570af3992256b2f40
Vitor Campos <vlfc.jogos@gmail.com>
2023-04-18 11:31:42 -0300

    Questão do Beecrowd

---
+++ Beecrowd 1052.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int mes;
+	cin >> mes;
+	if(mes == 1){
+		cout <<"January\n";
+	}
+	else if(mes == 2){
+		cout <<"February\n";
+	}
+	else if(mes == 3){
+		cout <<"March\n";
+	}
+	else if(mes == 4){
+		cout <<"April\n";
+	}
+	else if(mes == 5){
+		cout <<"May\n";
+	}
+	else if(mes == 6){
+		cout <<"June\n";
+	}
+	else if(mes == 7){
+		cout <<"July\n";
+	}
+	else if(mes == 8){
+		cout <<"August\n";
+	}
+	else if(mes == 9){
+		cout <<"September\n";
+	}
+	else if(mes == 10){
+		cout <<"October\n";
+	}
+	else if(mes == 11){
+		cout <<"November\n";
+	}
+	else if(mes == 12){
+		cout <<"December\n";
+	}
+	return 0;
+}
