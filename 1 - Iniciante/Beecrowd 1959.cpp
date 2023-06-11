c8bda52c25d2bdadb62bb88d16c110b6f3b7c571
Vitor Campos <vlfc.jogos@gmail.com>
2023-06-10 21:20:09 -0300

    Questão do Beecrowd

---
+++ 1 - Iniciante/Beecrowd 1959.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	unsigned long long lado, comprimento, resultado;
+	cin>>lado>>comprimento;
+	cout<<lado*comprimento<<"\n";
+    
+	return 0;
+}
