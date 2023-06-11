b73494874324aba2f4c686a34cb7309fb2cccb82
Vitor Campos <vlfc.jogos@gmail.com>
2023-06-10 21:25:02 -0300

    Questão do Beecrowd

---
+++ 1 - Iniciante/Beecrowd 1930.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int T1, T2, T3, T4, resultado;
+	cin>>T1>>T2>>T3>>T4;
+	resultado=(T1-1)+(T2-1)+(T3-1)+T4;
+	cout<<resultado<<"\n";
+    
+	return 0;
+}
