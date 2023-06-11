6f237420c826f6557ee1005fa8e02875f04d0277
Vitor Campos <vlfc.jogos@gmail.com>
2023-06-11 19:12:45 -0300

    Questão do Beecrowd

---
+++ 4 - Estruturas e Bibliotecas/Beecrowd 1763.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main(){
+	char nome[1000];
+	while(cin>>nome){
+		if(strcmp(nome, "brasil")==0){
+		cout<<"Feliz Natal!\n";
+	}
+		else if(strcmp(nome, "alemanha")==0){
+		cout<<"Frohliche Weihnachten!\n";
+	}
+		else if(strcmp(nome, "austria")==0){
+		cout<<"Frohe Weihnacht!\n";
+	}
+		else if(strcmp(nome, "coreia")==0){
+		cout<<"Chuk Sung Tan!\n";
+	}
+			else if(strcmp(nome, "espanha")==0){
+		cout<<"Feliz Navidad!\n";
+	}
+			else if(strcmp(nome, "grecia")==0){
+		cout<<"Kala Christougena!\n";
+	}
+			else if(strcmp(nome, "estados-unidos")==0){
+		cout<<"Merry Christmas!\n";
+	}
+			else if(strcmp(nome, "inglaterra")==0){
+		cout<<"Merry Christmas!\n";
+	}
+			else if(strcmp(nome, "australia")==0){
+		cout<<"Merry Christmas!\n";
+	}
+			else if(strcmp(nome, "portugal")==0){
+		cout<<"Feliz Natal!\n";
+	}
+			else if(strcmp(nome, "suecia")==0){
+		cout<<"God Jul!\n";
+	}
+			else if(strcmp(nome, "turquia")==0){
+		cout<<"Mutlu Noeller\n";
+	}
+			else if(strcmp(nome, "argentina")==0){
+		cout<<"Feliz Navidad!\n";
+	}
+			else if(strcmp(nome, "chile")==0){
+		cout<<"Feliz Navidad!\n";
+	}
+			else if(strcmp(nome, "mexico")==0){
+		cout<<"Feliz Navidad!\n";
+	}
+			else if(strcmp(nome, "antardida")==0){
+		cout<<"Merry Christmas!\n";
+	}
+			else if(strcmp(nome, "canada")==0){
+		cout<<"Merry Christmas!\n";
+	}
+			else if(strcmp(nome, "irlanda")==0){
+		cout<<"Nollaig Shona Dhuit!\n";
+	}
+			else if(strcmp(nome, "belgica")==0){
+		cout<<"Zalig Kerstfeest!\n";
+	}
+			else if(strcmp(nome, "italia")==0){
+		cout<<"Buon Natale!\n";
+	}
+			else if(strcmp(nome, "libia")==0){
+		cout<<"Buon Natale!\n";
+	}
+			else if(strcmp(nome, "siria")==0){
+		cout<<"Milad Mubarak!\n";
+	}
+			else if(strcmp(nome, "marrocos")==0){
+		cout<<"Milad Mubarak!\n";
+	}
+			else if(strcmp(nome, "japao")==0){
+		cout<<"Merii Kurisumasu!\n";
+	}
+	
+	else
+	cout<<"--- NOT FOUND ---\n";
+}
+	return 0;
+}
