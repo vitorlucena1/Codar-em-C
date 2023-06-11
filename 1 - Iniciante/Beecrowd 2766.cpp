5c7d806b545769e2a409d16049d8d3e3b5a2755d
Vitor Campos <vlfc.jogos@gmail.com>
2023-06-11 00:28:07 -0300

    Questão do Beecrowd

---
+++ 1 - Iniciante/Beecrowd 2766.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string nome;
+	
+	for(int i=0; i<10; i++){
+		cin>>nome;
+		if(i==2)
+		cout<<nome<<"\n";
+		
+		if(i==6)
+		cout<<nome<<"\n";
+		
+		if(i==8)
+		cout<<nome<<"\n";
+	}
+
+	return 0;
+}
