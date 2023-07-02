074afac9b89dda8057925586737d1583fa675cca
Vitor Campos <vlfc.jogos@gmail.com>
2023-07-01 23:21:01 -0300

    Questão do Beecrowd

---
+++ 1 - Iniciante/Beecrowd 2581.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int quant;
+	string texto;
+	
+	cin>>quant;
+	for(int i=0; i<quant; i++){
+	cin>>texto;
+	cout<<"I am Toorg!\n";
+	}
+
+	return 0;
+}
