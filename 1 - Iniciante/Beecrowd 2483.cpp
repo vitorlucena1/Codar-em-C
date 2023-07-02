bdeba8bffc3f3e0201d050612fde89c092974071
Vitor Campos <vlfc.jogos@gmail.com>
2023-07-02 00:59:18 -0300

    Questão do Beecrowd

---
+++ 1 - Iniciante/Beecrowd 2483.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int quant;
+	cin>>quant;
+
+	cout<<"Feliz nat";
+
+	for(int i=0; i<quant; i++){
+	cout<<"a";
+	}
+	
+	cout<<"l!\n";
+	
+	return 0;
+}
