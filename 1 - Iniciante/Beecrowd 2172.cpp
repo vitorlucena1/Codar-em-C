c1b46a7b581f8237998df97d7013b0583fbb757a
Vitor Campos <vlfc.jogos@gmail.com>
2023-07-03 00:57:55 -0300

    Questão do Beecrowd

---
+++ 1 - Iniciante/Beecrowd 2172.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num1, num2;
+	while(cin>>num1>>num2){
+
+	if(num1==0 && num2==0)
+	break;
+
+	else
+	cout<<num1*num2<<"\n";
+	}
+	
+	return 0;
+}
