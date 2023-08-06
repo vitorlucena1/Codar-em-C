892fa3ecbc37fb5028835951438192aa1aac4e6e
Vitor <vlfc.jogos@gmail.com>
2023-08-06 12:57:14 -0300

    Questão do Beecrowd

---
+++ 1 - Iniciante/Beecrowd 2753.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int numero=97;
+
+for(char i='a'; i<='z'; i++){
+    cout<<numero<<" e "<<i<<endl;
+    numero++;
+}
+
+return 0;
+}
---
+++ 1 - Iniciante/Beecrowd 2755.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"\"Ro'b'er	to\\/\""<<endl;
+    cout<<"(._.) ( l: ) ( .-. ) ( :l ) (._.)"<<endl;
+    cout<<"(^_-) (-_-) (-_^)"<<endl;
+    cout<<"(\"_\") ('.')"<<endl;
+
+return 0;
+}
