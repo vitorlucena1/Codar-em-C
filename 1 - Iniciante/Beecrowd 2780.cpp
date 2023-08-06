4a3ab4b18aaa8ff2a9bdeacab11417c4b80fe732
Vitor <vlfc.jogos@gmail.com>
2023-08-06 13:26:38 -0300

    Questão do Beecrowd

---
+++ 1 - Iniciante/Beecrowd 2780.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int num;
+cin>>num;
+
+if(num<=800)
+cout<<"1"<<endl;
+
+if(num>800 && num<=1400)
+cout<<"2"<<endl;
+
+if(num>1400 && num<=2000)
+cout<<"3"<<endl;
+
+return 0;
+}
