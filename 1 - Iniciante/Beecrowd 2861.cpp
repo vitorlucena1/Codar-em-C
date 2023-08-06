d5ece59cb1918d696853456b97b86452629f0a4f
Vitor <vlfc.jogos@gmail.com>
2023-08-06 13:18:04 -0300

    Questão do Beecrowd

---
+++ 1 - Iniciante/Beecrowd 2861.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int quant;
+cin>>quant;
+
+fflush(stdin);
+
+for(int i=0; i<quant; i++){
+	string pergunta;
+	getline(cin, pergunta);
+
+	cout<<"gzuz"<<endl;
+}
+
+	return 0;
+}
